6a09e30f8a871054eab0ab44e023e6941df1bd05
Colin Scott <colins@pageuppeople.com>
2016-03-04 12:19:24 +1100

    Command line tools for encrypting and decrypting

---
+++ src/SimpleEncrypt.Decryptor/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using NDesk.Options;
+
+namespace SimpleEncrypt.Decryptor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var decryptorArgs = DecryptorArgs.Parse(args);
+            if (decryptorArgs == null)
+            {
+                return;
+            }
+
+            Console.WriteLine(SimpleEncryptClient.Decrypt(decryptorArgs.Value));
+        }
+    }
+
+    public class DecryptorArgs
+    {
+        public string Region { get; private set; }
+        public string Value { get; private set; }
+
+        public static DecryptorArgs Parse(string[] args)
+        {
+            string region = null;
+            string value = null;
+            var displayHelp = false;
+
+            var options = new OptionSet
+            {
+                {
+                    "r|region=",
+                    "The {AWSREGION} that contains the key",
+                    v => region = v
+                },
+                {
+                    "v|value=",
+                    "The {VALUE} to decrypt",
+                    v => value = v
+                },
+                {
+                    "help",
+                    "Show this message and exit.",
+                    v => displayHelp = true
+                }
+            };
+
+            try
+            {
+                options.Parse(args);
+            }
+            catch (OptionException ex)
+            {
+                DisplayErrorMessage(ex.Message);
+                return null;
+            }
+
+            if (displayHelp)
+            {
+                DisplayHelp(options);
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(region))
+            {
+                DisplayErrorMessage("Must supply an AWS Region");
+                return null;
+            }
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                DisplayErrorMessage("Must supply a value to decrypt");
+                return null;
+            }
+
+            return new DecryptorArgs
+            {
+                Region = region,
+                Value = value
+            };
+        }
+
+        private static void DisplayErrorMessage(string message)
+        {
+            Console.Write("SimpleEncrypt.Decryptor: ");
+            Console.WriteLine(message);
+            Console.WriteLine("Try `SimpleEncrypt.Decryptor --help' for more information.");
+        }
+
+        private static void DisplayHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: SimpleEncrypt.Encryptor [OPTIONS]");
+            Console.WriteLine("Encrypt a value against an AWS key.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
