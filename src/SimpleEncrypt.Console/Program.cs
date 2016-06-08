d3d4ac18ca6552156724d71ea9a4799245c2d537
P Karthikeyan <karthi.email@gmail.com>
2016-06-08 23:43:48 +1000

    working version using microsoft command line util

--- src/SimpleEncrypt.Console/Program.cs
+++ src/SimpleEncrypt.Console/Program.cs
@@ -1,57 +1,108 @@
 ﻿using System;
-using CommandLine;
+using Microsoft.Extensions.CommandLineUtils;
 using static System.Console;
 
 namespace SimpleEncrypt.Console
 {
     public class Program
     {
+
+        private readonly CommandLineApplication app = null;
+
+        public Program()
+        {
+            CommandArgument key, secret, region, awskey, awsSecret, awsToken;
+            
+            app = new CommandLineApplication {Description = "Simple encryption console using kms"};
+
+            app.Command("encrypt", c =>
+            {
+                key = c.Argument("key", "aws kms key");
+                secret = c.Argument("value", "value to be encrypted");
+                region = c.Argument("region", "region where aws kms key is located");
+                awskey = c.Argument("awskey", "aws access key");
+                awsSecret = c.Argument("awsSecret", "aws access secret");
+                awsToken = c.Argument("awsToken", "aws access session token");
+
+                c.OnExecute(async () =>
+                {
+                    await secret.Value.EncryptAsync(key.Value, region.Value, awskey.Value, awsSecret.Value, awsToken.Value);
+                    return 0;
+                });
+            });
+
+            app.Command("decrypt", c =>
+            {
+                secret = c.Argument("value", "value to be encrypted");
+                region = c.Argument("region", "region where aws kms key is located");
+                awskey = c.Argument("awskey", "aws access key");
+                awsSecret = c.Argument("awsSecret", "aws access secret");
+                awsToken = c.Argument("awsToken", "aws access session token");
+                
+
+                c.OnExecute(async () =>
+                {
+                    await secret.Value.DecryptAsync(region.Value, awskey.Value, awsSecret.Value, awsToken.Value);
+                    return 0;
+                });
+            });
+        }
+
+        public int Execute(string[] args)
+        {
+            return app.Execute(args);
+        }
+
+        public void ShowHelp()
+        {
+            app.ShowHelp();
+        }
+
         public static void Main(string[] args)
         {
+            var program = new Program();
             try
             {
-                Parser.Default.ParseArguments<EncryptOptions, DecryptOptions>(args)
-                    .WithParsed<EncryptOptions>(Encrypt)
-                    .WithParsed<DecryptOptions>(Decrypt)
-                    .WithNotParsed(errs => { });
+                program.Execute(args);
             }
-            catch (Exception e)
+            catch (Exception exception)
             {
-                WriteLine(e.Message);
+                WriteLine(exception.Message);
+                program.ShowHelp();
             }
         }
 
-        private static void Decrypt(DecryptOptions opts)
-        {
-            WriteLine(opts.Value.Decrypt(opts.Region));
-        }
-
-        private static void Encrypt(EncryptOptions opts)
-        {
-            WriteLine(opts.Value.Encrypt(opts.KeyId, opts.Region));
-        }
+//        private static void DecryptAsync(DecryptOptions opts)
+//        {
+//            WriteLine(opts.Value.DecryptAsync(opts.Region));
+//        }
+//
+//        private static void EncryptAsync(EncryptOptions opts)
+//        {
+//            WriteLine(opts.Value.EncryptAsync(opts.KeyId, opts.Region));
+//        }
     }
 
-    internal class Options
-    {
-        [Option('r', "region", HelpText = "The {AWSREGION} that contains the key", Required = true)]
-        public string Region { get; set; }
-    }
-
-    [Verb("encrypt", HelpText = "encrypts a value using the key")]
-    internal class EncryptOptions: Options
-    {
-        [Option('k', "keyId", HelpText = "The {KEYID} to encrypt the value against", Required = true)]
-        public string KeyId { get; set; }
-
-        [Option('v', "value", HelpText = "The {VALUE} to encrypt", Required = true)]
-        public string Value { get; set; }
-    }
-
-    [Verb("decrypt", HelpText = "decrypts value")]
-    internal class DecryptOptions: Options
-    {
-        [Option('v', "value", HelpText = "The {VALUE} to encrypt", Required = true)]
-        public string Value { get; set; }
-    }
+//    internal class Options
+//    {
+//        [Option('r', "region", HelpText = "The {AWSREGION} that contains the key", Required = true)]
+//        public string Region { get; set; }
+//    }
+//
+//    [Verb("encrypt", HelpText = "encrypts a value using the key")]
+//    internal class EncryptOptions: Options
+//    {
+//        [Option('k', "keyId", HelpText = "The {KEYID} to encrypt the value against", Required = true)]
+//        public string KeyId { get; set; }
+//
+//        [Option('v', "value", HelpText = "The {VALUE} to encrypt", Required = true)]
+//        public string Value { get; set; }
+//    }
+//
+//    [Verb("decrypt", HelpText = "decrypts value")]
+//    internal class DecryptOptions: Options
+//    {
+//        [Option('v', "value", HelpText = "The {VALUE} to encrypt", Required = true)]
+//        public string Value { get; set; }
+//    }
 }
--- src/SimpleEncrypt/SimpleEncryptClient.cs
+++ src/SimpleEncrypt/SimpleEncryptClient.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using Amazon;
 using Amazon.KeyManagementService;
 using Amazon.KeyManagementService.Model;
+using Amazon.Runtime;
 
 namespace SimpleEncrypt
 {
     public static class SimpleEncryptClient
     {
-        public static string Decrypt(this string encryptedValue, string regionName)
+        public static async Task<string> DecryptAsync(this string encryptedValue, string regionName, string awsKey, string awsSecret, string awsToken)
         {
-            var client = new AmazonKeyManagementServiceClient(RegionEndpoint.GetBySystemName(regionName));
+            var client = new AmazonKeyManagementServiceClient(new SessionAWSCredentials(awsKey, awsSecret, awsToken), RegionEndpoint.GetBySystemName(regionName));
 
             var ciphertestStream = new MemoryStream(Convert.FromBase64String(encryptedValue)) { Position = 0 };
 
             var decryptRequest = new DecryptRequest { CiphertextBlob = ciphertestStream };
 
-            var response = client.Decrypt(decryptRequest);
+            var response = await client.DecryptAsync(decryptRequest);
 
             var buffer = new byte[response.ContentLength];
 
@@ -26,9 +28,9 @@ namespace SimpleEncrypt
             return Encoding.UTF8.GetString(buffer);
         }
 
-        public static string Encrypt(this string value, string key, string regionName)
+        public static async Task<string> EncryptAsync(this string value, string key, string regionName, string awsKey, string awsSecret, string awsToken)
         {
-            var client = new AmazonKeyManagementServiceClient(RegionEndpoint.GetBySystemName(regionName));
+            var client = new AmazonKeyManagementServiceClient(new SessionAWSCredentials(awsKey, awsSecret, awsToken), RegionEndpoint.GetBySystemName(regionName));
 
             var plaintextData = new MemoryStream(Encoding.UTF8.GetBytes(value))
             {
@@ -41,7 +43,7 @@ namespace SimpleEncrypt
                 Plaintext = plaintextData
             };
 
-            var response = client.Encrypt(encryptRequest);
+            var response = await client.EncryptAsync(encryptRequest);
 
             var buffer = new byte[response.CiphertextBlob.Length];
 
